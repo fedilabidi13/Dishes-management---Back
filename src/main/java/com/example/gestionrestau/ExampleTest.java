012b8500de7da56de40a7e8e77b334c0d4dfcc56
Fedi Labidi <92761733+fedilabidi13@users.noreply.github.com>
2024-10-22 01:05:56 +0100

    az

---
+++ src/main/java/com/example/gestionrestau/ExampleTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void testAdditssion() {
+        assertEquals(1, 1 * 1);
+    }
+    
+}
